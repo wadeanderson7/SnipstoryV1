6f980c4ab95a5695448c5b74d27071c9ce01f608
derek <derek@pixelture.com>
2013-12-11 16:01:32 -0700

    Add page edit/delete options. Improved page handling when deleting and editing items on page.

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -21,7 +21,13 @@ snipStoryControllers.controller('EditorCtrl', ['$scope', '$http', '$modal', 'ima
     $scope.numUploading = imageHandler.numUploading;
     
     $scope.setChapter = function setChapter(idx) {
-    	$scope.curChapter = $scope.story.chapters[idx];
+    	$scope.curChapter.lastPageIdx = $scope.pageIdx; 
+    	var chapter = $scope.story.chapters[idx];
+    	$scope.curChapter = chapter;
+    	if (chapter.lastPageIdx !== undefined)
+    		$scope.pageIdx = chapter.lastPageIdx;
+    	else
+    		$scope.pageIdx = 0;
     };
     
     $scope.setPageIdx = function setPageIdx(idx) {
@@ -43,26 +49,40 @@ snipStoryControllers.controller('EditorCtrl', ['$scope', '$http', '$modal', 'ima
     	return imageHandler.pics[imageId.toString()][thumbType];
     };
     
-    $scope.deleteItem = function deleteItem(itemId, itemIdx) {
-    	var deleteConfirmModal = $modal.open({
-    		templateUrl: 'confirmDeleteSnippet.html',
-    		controller: 'ConfirmDeleteSnippetCtrl',
+    $scope.addItem = function() {
+    	var addSnippetModal = $modal.open({
+    		templateUrl: 'addSnippetDialog.html',
+    		controller: 'AddSnippetDialogCtrl',
+    		backdrop: 'static'
     	});
-    	var pageIdx = $scope.pageIdx;
-    	deleteConfirmModal.result.then(function() {
-        	imageHandler.incUploads();
-    		$scope.curChapter.pages[pageIdx].items.splice(itemIdx, 1);
-    		$http.delete('/items/' + itemId)
+    	
+    	addSnippetModal.result.then(function (result) {   		
+    		//create item
+    		var pageIdx = $scope.pageIdx;
+    		var page = $scope.curChapter.pages[pageIdx];
+    		var items = page.items;
+    		var ordering = ORDER_INTERVAL;
+    		if (items.length > 0)
+    			ordering += items[items.length - 1].ordering;
+    		var newItem = {"description":result.caption, "type":"IMAGE", "ordering":ordering};
+    		$http.post('/pages/' + page.id + '/item', newItem, {})
 			.success(function(data, status, headers, config) {
-				imageHandler.decUploads();
+				data.localImageId = result.picId;
+				$scope.curChapter.pages[pageIdx].items.push(data);
+				//add waiting image to item or add item to list of items waiting for pics to upload
+				if (!imageHandler.assignWaitingPicToItem(data.id)) {
+					imageHandler.addItemWaitingForPic(result.picId, data.id);
+				}
+				
 			}).error(function(data, status, headers, config) {
 				//TODO: add error handler of some sort
 			});
+    		
     	});
+    	
     };
     
-    $scope.editItem = function editItem(itemId, itemIdx) {
-    	var pageIdx = $scope.pageIdx;
+    $scope.editItem = function editItem(pageIdx, itemIdx, itemId) {
     	var item = $scope.curChapter.pages[pageIdx].items[itemIdx];
     	var editConfirmModal = $modal.open({
     		templateUrl: 'editSnippetDialog.html',
@@ -99,39 +119,23 @@ snipStoryControllers.controller('EditorCtrl', ['$scope', '$http', '$modal', 'ima
 				//TODO: add error handler of some sort
 			});
     	}); 	
-    }
+    };
     
-    $scope.addItem = function() {
-    	var addSnippetModal = $modal.open({
-    		templateUrl: 'addSnippetDialog.html',
-    		controller: 'AddSnippetDialogCtrl',
-    		backdrop: 'static'
+    $scope.deleteItem = function deleteItem(pageIdx, itemIdx, itemId) {
+    	var deleteConfirmModal = $modal.open({
+    		templateUrl: 'confirmDeleteSnippet.html',
+    		controller: 'ConfirmCtrl',
     	});
-    	
-    	addSnippetModal.result.then(function (result) {   		
-    		//create item
-    		var pageIdx = $scope.pageIdx;
-    		var page = $scope.curChapter.pages[pageIdx];
-    		var items = page.items;
-    		var ordering = ORDER_INTERVAL;
-    		if (items.length > 0)
-    			ordering += items[items.length - 1].ordering;
-    		var newItem = {"description":result.caption, "type":"IMAGE", "ordering":ordering};
-    		$http.post('/pages/' + page.id + '/item', newItem, {})
+    	deleteConfirmModal.result.then(function() {
+        	imageHandler.incUploads();
+    		$scope.curChapter.pages[pageIdx].items.splice(itemIdx, 1);
+    		$http.delete('/items/' + itemId)
 			.success(function(data, status, headers, config) {
-				data.localImageId = result.picId;
-				$scope.curChapter.pages[pageIdx].items.push(data);
-				//add waiting image to item or add item to list of items waiting for pics to upload
-				if (!imageHandler.assignWaitingPicToItem(data.id)) {
-					imageHandler.addItemWaitingForPic(result.picId, data.id);
-				}
-				
+				imageHandler.decUploads();
 			}).error(function(data, status, headers, config) {
 				//TODO: add error handler of some sort
 			});
-    		
     	});
-    	
     };
     
     $scope.addPage = function() {
@@ -159,6 +163,67 @@ snipStoryControllers.controller('EditorCtrl', ['$scope', '$http', '$modal', 'ima
 				//TODO: add error handler of some sort
 			});
     		
+    	});
+    };
+    
+    $scope.editPage = function editPage(pageId, pageIdx) {
+    	var page = $scope.curChapter.pages[pageIdx];
+    	var editModal = $modal.open({
+    		templateUrl: 'editPageDialog.html',
+    		controller: 'EditPageDialogCtrl',
+    		resolve: {
+    	        name: function() { return page.name; },
+    	        description: function() { return page.description; }
+    	      },
+    	      backdrop: 'static'
+    	});
+    	editModal.result.then(function(result) {
+        	imageHandler.incUploads();
+        	page.name = result.name;
+        	page.description = result.description;
+    		var data = {"name":result.name, "description":result.description};
+    		$http.post('/pages/' + pageId, data)
+			.success(function(data, status, headers, config) {
+				imageHandler.decUploads();
+			}).error(function(data, status, headers, config) {
+				//TODO: add error handler of some sort
+			});
+    	}); 	
+    };
+    
+    $scope.deletePage = function deletePage(pageId, pageIdx) {
+    	var pages = $scope.curChapter.pages;
+    	
+    	if (pages.length == 1)
+    		return; //can't delete last page
+    	
+    	var deleteConfirmModal = $modal.open({
+    		templateUrl: 'confirmDeletePage.html',
+    		controller: 'ConfirmCtrl',
+    	});
+    	deleteConfirmModal.result.then(function() {
+        	imageHandler.incUploads();
+        	
+        	//handle changing current page index
+        	
+        	var curPage = pages[pageIdx];
+        	if (curPage.id == pageId) {
+        		if (pageIdx == pages.length-1)
+        			curPage = pages[pageIdx-1];
+        		else
+        			curPage = pages[pageIdx+1];
+        	} 
+    		pages.splice(pageIdx, 1);
+    		
+    		//find new pageIdx
+    		$scope.pageIdx = pages.indexOf(curPage);
+    		
+    		$http.delete('/pages/' + pageId)
+			.success(function(data, status, headers, config) {
+				imageHandler.decUploads();
+			}).error(function(data, status, headers, config) {
+				//TODO: add error handler of some sort
+			});
     	});
     };
     
@@ -220,28 +285,36 @@ snipStoryControllers.controller('EditSnippetCtrl', ['$scope', '$modalInstance', 
  	};
  }]);
 
-snipStoryControllers.controller('ConfirmDeleteSnippetCtrl', ['$scope', '$modalInstance',
- 	function($scope, $modalInstance) {
- 	
- 	$scope.ok = function() {
- 		$modalInstance.close();
- 	};
- 	$scope.cancel = function() {
- 		$modalInstance.dismiss('cancel');
- 	};
- }]);
+snipStoryControllers.controller('ConfirmCtrl', ['$scope', '$modalInstance',
+	function($scope, $modalInstance) {
+	$scope.ok = function() { $modalInstance.close(); };
+	$scope.cancel = function() { $modalInstance.dismiss('cancel'); };
+  }]);
 
 snipStoryControllers.controller('AddPageDialogCtrl', ['$scope', '$modalInstance',
  	function($scope, $modalInstance) {
 
+	$scope.name = null;
  	$scope.description = null;
- 	$scope.name = null;
  	 	
  	$scope.ok = function() {
  		$modalInstance.close({description:$scope.description, name:$scope.name});
  	};
  	$scope.cancel = function() {
- 		//TODO: if picture is uploaded, delete it - if it is queued for upload, queue if for deletion
  		$modalInstance.dismiss('cancel');
  	};
  }]);
+
+snipStoryControllers.controller('EditPageDialogCtrl', ['$scope', '$modalInstance', 'name', 'description',
+   	function($scope, $modalInstance, name, description) {
+
+	$scope.name = name;
+   	$scope.description = description;
+   	 	
+   	$scope.ok = function() {
+   		$modalInstance.close({description:$scope.description, name:$scope.name});
+   	};
+   	$scope.cancel = function() {
+   		$modalInstance.dismiss('cancel');
+   	};
+}]);
